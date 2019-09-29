d299ef777573bdaaba4ad093a0237952fe98f6ae
De Carvalho Gabriel <gabriel.de_carvalho.ext@nokia.com>
2019-09-29 18:31:02 -0500

    Structure components, style MenuItem components

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.scss'
+import Directory from '../components/Directory'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+)
+
+export default HomePage
